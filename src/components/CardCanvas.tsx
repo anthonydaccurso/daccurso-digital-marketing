fd939454ee9c4f4f5ecd1247f905a541e1794f86
Anthony Daccurso <153415273+anthonydaccurso@users.noreply.github.com>
2025-11-04 01:12:08 -0500

    Updated index.html

--- src/components/CardCanvas.tsx
+++ src/components/CardCanvas.tsx
@@ -89,10 +89,10 @@ const Card = () => {
         <div className={`text-white ${isMobile ? 'w-[336px]' : 'w-[310px]'} text-sm select-none`}>
           <div className="flex items-center gap-4 mb-2">
             <img
-              src="https://bvevrurqtidadhfsuoee.supabase.co/storage/v1/object/public/media//anthony-daccurso-64x64.webp?quality=80&width=64"
+              src="https://bvevrurqtidadhfsuoee.supabase.co/storage/v1/object/public/media//anthony-daccurso-64x64.webp?quality=75&width=64"
               srcSet="
-                https://bvevrurqtidadhfsuoee.supabase.co/storage/v1/object/public/media//anthony-daccurso-64x64.webp?quality=80&width=64 1x,
-                https://bvevrurqtidadhfsuoee.supabase.co/storage/v1/object/public/media//anthony-daccurso-128x128.webp?quality=80&width=128 2x
+                https://bvevrurqtidadhfsuoee.supabase.co/storage/v1/object/public/media//anthony-daccurso-64x64.webp?quality=75&width=64 1x,
+                https://bvevrurqtidadhfsuoee.supabase.co/storage/v1/object/public/media//anthony-daccurso-128x128.webp?quality=75&width=128 2x
               "
               alt="Anthony Daccurso"
               width={40}

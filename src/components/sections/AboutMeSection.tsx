46e9752213950bcde3986f32903d6a93a347ddb5
Anthony Daccurso <153415273+anthonydaccurso@users.noreply.github.com>
2025-11-06 17:06:21 -0500

    Updated AboutMeSection.tsx

--- src/components/sections/AboutMeSection.tsx
+++ src/components/sections/AboutMeSection.tsx
@@ -6,7 +6,7 @@ function AboutMeSection() {
   return (
     <div className="w-full">
       <Helmet>
-        <title>Anthony Daccurso | Digital Marketing & Web Development</title>
+        <title>Anthony Daccurso | Daccurso Digital Marketing</title>
         <meta
           name="description"
           content="I'm Anthony Daccurso, a Digital Marketing & SEO Specialist passionate about web design, performance optimization, and helping businesses grow their digital presence."

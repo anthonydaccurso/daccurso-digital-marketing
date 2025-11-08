d00f2c78f0d8228e69611322d00c8cf46ff6b2b4
Anthony Daccurso <153415273+anthonydaccurso@users.noreply.github.com>
2025-11-08 15:36:44 -0500

    Updated ask-ant.ts

--- src/pages/api/ask-ant.ts
+++ src/pages/api/ask-ant.ts
@@ -154,7 +154,7 @@ export const askAnt = async (userMessage: string) => {
         'HTTP-Referer': 'https://anthonydaccurso.com',
       },
       body: JSON.stringify({
-        model: 'nousresearch/nous-hermes-2-mistral:free',
+        model: 'microsoft/phi-3-mini-128k-instruct:free',
         max_tokens: 300,
         messages: [
           {
@@ -176,7 +176,7 @@ export const askAnt = async (userMessage: string) => {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        model: 'nousresearch/nous-hermes-2-mistral:free',
+        model: 'microsoft/phi-3-mini-128k-instruct:free',
         max_tokens: 300,
         messages: [
           {

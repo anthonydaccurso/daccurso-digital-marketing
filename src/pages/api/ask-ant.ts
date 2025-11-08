47c558419c9850e2c2ea8b940676b110c12e22c5
Anthony Daccurso <153415273+anthonydaccurso@users.noreply.github.com>
2025-11-08 15:23:53 -0500

    Updated ask-ant.ts

--- src/pages/api/ask-ant.ts
+++ src/pages/api/ask-ant.ts
@@ -155,7 +155,7 @@ export const askAnt = async (userMessage: string) => {
       },
       body: JSON.stringify({
         model: 'mistralai/mistral-7b-instruct:free',
-        max_tokens: 150,
+        max_tokens: 300,
         messages: [
           {
             role: 'system',

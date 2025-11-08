eeeb965eba295e0b67df17673ba16304075e4579
Anthony Daccurso <153415273+anthonydaccurso@users.noreply.github.com>
2025-11-08 05:39:09 -0500

    Updated CardCanvas.tsx

--- src/components/CardCanvas.tsx
+++ src/components/CardCanvas.tsx
@@ -89,8 +89,7 @@ const Card = () => {
         <div className={`text-white ${isMobile ? 'w-[336px]' : 'w-[310px]'} text-sm select-none`}>
           <div className="flex items-center gap-4 mb-2">
             <img
-              src="https://bvevrurqtidadhfsuoee.supabase.co/storage/v1/object/public/media/anthony-daccurso-128x128.webp?width=128&quality=70&format=webp
-&resize=cover"
+              src="https://bvevrurqtidadhfsuoee.supabase.co/storage/v1/object/public/media/anthony-daccurso-128x128.webp?width=128&quality=70&format=webp&resize=cover"
               srcSet="
                 https://bvevrurqtidadhfsuoee.supabase.co/storage/v1/object/public/media/anthony-daccurso-64x64.webp?width=64&quality=70&format=webp&resize=cover 1x,
                 https://bvevrurqtidadhfsuoee.supabase.co/storage/v1/object/public/media/anthony-daccurso-128x128.webp?width=128&quality=70&format=webp&resize=cover 2x
@@ -103,6 +102,7 @@ const Card = () => {
               className="w-10 h-10 rounded-full object-cover"
               draggable={false}
             />
+
             <div>
               <h2 className="font-semibold text-lg select-none">Anthony Daccurso</h2>
               <p className="text-[13px] text-gray-300 select-none whitespace-nowrap">

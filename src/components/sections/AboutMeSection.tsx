f6d17a62a0401085d5002e414c252fd2d1d210b8
Anthony Daccurso <153415273+anthonydaccurso@users.noreply.github.com>
2026-04-18 13:00:35 -0400

    Updated AboutMeSection.tsx

--- src/components/sections/AboutMeSection.tsx
+++ src/components/sections/AboutMeSection.tsx
@@ -119,7 +119,7 @@ function AboutMeSection() {
               loading="eager"
               fetchpriority="high"
               decoding="async"
-              className="w-full h-full object-cover"
+              className="w-full h-full object-cover brightness-40"
             />
           </motion.div>
 
